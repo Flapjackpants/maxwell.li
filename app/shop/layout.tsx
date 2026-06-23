f1ed0d8fbbceaeb60c09b2fbb148f75d9e74edef
Maxwell Li <mkl96@cornell.edu>
2026-06-23 17:05:40 -0400

    chore: fixed title and subtitles

--- app/shop/layout.tsx
+++ app/shop/layout.tsx
@@ -4,7 +4,7 @@ import { ShopBackgroundAudio } from "./components/ShopBackgroundAudio";
 
 export const metadata: Metadata = {
   title: "umazing",
-  description: "the most UMAZING shopping experience",
+  description: "Or head to the Susland Plaza (-266, 92, -288) for our brick-and-mortar shopping experience!", 
 };
 
 export default function ShopLayout({
